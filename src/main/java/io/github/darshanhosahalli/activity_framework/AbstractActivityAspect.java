1ace52347fef4a7815bd02fffc3b0885abbd3528
darshan bs <darshanbs@darshans-MacBook-Air.local>
2024-12-28 18:28:56 +0530

    completed the frameword

--- src/main/java/io/github/darshanhosahalli/activity_framework/AbstractActivityAspect.java
+++ src/main/java/io/github/darshanhosahalli/activity_framework/AbstractActivityAspect.java
@@ -5,30 +5,69 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.core.annotation.Order;
 
+/**
+ * Abstract aspect for handling activities.
+ *
+ * This aspect provides common functionality for before, afterReturning, afterThrowing, and around advice for methods annotated with the {@link Activity} annotation.
+ */
 @Aspect
 @Order(1)
 public class AbstractActivityAspect {
 
+    /**
+     * Before advice for methods annotated with {@link Activity}.
+     *
+     * @param joinPoint the join point
+     * @param activity the activity annotation
+     */
     @Before(value = "@annotation(activity)", argNames = "activity")
     public void methodForBefore(JoinPoint joinPoint, Activity activity) {
         this.before(activity);
     }
 
+    /**
+     * After returning advice for methods annotated with {@link Activity}.
+     *
+     * @param joinPoint the join point
+     * @param activity the activity annotation
+     * @param returnValue the return value of the method
+     */
     @AfterReturning(value="@annotation(activity)", returning = "returnValue")
     public void methodForAfterReturning(JoinPoint joinPoint, Activity activity, Object returnValue) {
         this.afterReturning(activity, returnValue);
     }
 
+    /**
+     * After throwing advice for methods annotated with {@link Activity}.
+     *
+     * @param joinPoint the join point
+     * @param activity the activity annotation
+     * @param ex the exception thrown by the method
+     */
     @AfterThrowing(pointcut="@annotation(activity)", throwing = "ex")
     public void methodForAfterThrowing(JoinPoint joinPoint, Activity activity, Exception ex) {
         this.afterThrowing(activity, ex);
     }
 
+    /**
+     * After advice for methods annotated with {@link Activity}.
+     *
+     * @param joinPoint the join point
+     * @param activity the activity annotation
+     */
     @After("@annotation(activity)")
     public void methodForAfter(JoinPoint joinPoint, Activity activity) {
         this.after(activity);
     }
 
+    /**
+     * Around advice for methods annotated with {@link Activity}.
+     *
+     * @param pjp the proceeding join point
+     * @param activity the activity annotation
+     * @return the return value of the method
+     * @throws Throwable any exception thrown by the method
+     */
     @Around("@annotation(activity)")
     public String methodForAround(ProceedingJoinPoint pjp, Activity activity) throws Throwable {
         try {
@@ -42,16 +81,49 @@ public class AbstractActivityAspect {
         }
     }
 
+    /**
+     * Abstract method for before advice implementation.
+     *
+     * @param activity the activity annotation
+     */
     public void before(Activity activity) {};
 
+    /**
+     * Abstract method for after returning advice implementation.
+     *
+     * @param activity the activity annotation
+     * @param returnValue the return value of the method
+     */
     public void afterReturning(Activity activity, Object returnValue) {};
 
+    /**
+     * Abstract method for after throwing advice implementation.
+     *
+     * @param activity the activity annotation
+     * @param error the exception thrown by the method
+     */
     public void afterThrowing(Activity activity, Exception error) {};
 
+    /**
+     * Abstract method for after advice implementation.
+     *
+     * @param activity the activity annotation
+     */
     public void after(Activity activity) {};
 
+    /**
+     * Abstract method for around advice implementation (before).
+     *
+     * @param activity the activity annotation
+     */
     public void around(Activity activity) {};
 
+    /**
+     * Abstract method for around advice implementation (after).
+     *
+     * @param activity the activity annotation
+     * @param error the exception thrown by the method
+     */
     public void around(Activity activity, Exception error) {};
 
 }
--- src/main/java/io/github/darshanhosahalli/activity_framework/Activity.java
+++ src/main/java/io/github/darshanhosahalli/activity_framework/Activity.java
@@ -6,14 +6,26 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Hello world!
+ * This annotation is used to mark a method as an activity.
  *
+ * @author darshan
+ * @version 1.0
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Activity {
 
+    /**
+     * The name of the activity.
+     *
+     * @return The activity name.
+     */
     String activity();
 
+    /**
+     * The type of the activity.
+     *
+     * @return The activity type.
+     */
     Class<?> type() default String.class;
 }
